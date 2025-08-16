0d703a8c5879b7980ed7e63ab545cfec1124bd4f
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2025-08-16 18:09:52 +0530

    hello world

---
+++ 1_hello_world/main.go
@@ -0,0 +1,7 @@
+package main // This is the main package, it is the entry point of the application
+
+import "fmt" // Importing the fmt package for formatted I/O
+
+func main() {
+	fmt.Println("hello world") // Printing "hello world" to the console
+}
